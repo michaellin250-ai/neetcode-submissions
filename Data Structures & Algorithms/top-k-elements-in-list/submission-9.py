5b7473a0ed4675b9d0801c54183730db76b4a6fe
Michael Lin <michaellin250@gmail.com>
2026-06-19 11:25:04 -0700

    Add: top-k-elements-in-list - submission-9

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-9.py
@@ -0,0 +1,14 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        count = defaultdict(int)
+        for num in nums:
+            count[num] += 1
+        
+        res = sorted(count.keys(), key = lambda x: count[x], reverse = True)
+        return res[:k]
+        #1.Count.keys() gives all unique numbers
+        #2. key = lambda x: count[x] tells python: "When comparing two numbers, compare their frequencies instead."
+        #3. Sorted() orders them by those frequencies
+        #4. Reverse=True makes the highest frequencies come first 
+        #5. res[:k] grabs the first k most frequent elements 
+        
