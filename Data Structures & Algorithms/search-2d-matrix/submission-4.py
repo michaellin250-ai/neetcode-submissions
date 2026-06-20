fecba3d90dba82cb6ddc647e89d7f4570bce41c3
Michael Lin <michaellin250@gmail.com>
2026-06-20 16:10:24 -0700

    Add: search-2d-matrix - submission-4

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-4.py
@@ -0,0 +1,33 @@
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        rows = len(matrix) #3
+        cols = len(matrix[0]) #4
+        L, R = 0, (rows * cols) - 1 #11 
+        
+        while L <= R:
+            mid = (L + R) // 2 #11 / 2 = 5 
+            row = mid // cols #5 / 4 = 1 
+            col = mid % cols  #5 / 4 = 1
+
+            if matrix[row][col] < target:
+                L = mid + 1
+            elif matrix[row][col] > target:
+                R = mid - 1
+            else:
+                return True 
+        return False 
+
+
+
+
+            #Our mid point 
+            #What row are we at? 
+              #What col are we at? 
+
+             #If our value at that specific part in the matrix is less then the target, increase our left pointer 
+                
+             #If our value at the specific part in the matrix is greater then the target, increase our right pointer 
+                
+           
+                #If all goes well, return true but if not then return False
+       
